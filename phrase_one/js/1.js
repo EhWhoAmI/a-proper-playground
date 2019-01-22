ab31aeaf4700b928e2ff6aa8488d0887bfb6ec1f
samuel <chensrj2@gmail.com>
2019-01-21 19:02:47 -0600

    Update 1.js

--- phrase_one/js/1.js
+++ phrase_one/js/1.js
@@ -3,6 +3,9 @@ const canvasx = grid[0]*50;
 const canvasy = grid[1]*50;
 var foods = [];
 var bots = [];
+var worldmap = new Array(grid[0]*grid[1]).fill(0);
+const arrSum = arr => arr.reduce((a,b) => a + b, 0)
+
 function setup() {
   createCanvas(canvasx,canvasy);
 
@@ -15,13 +18,18 @@ function setup() {
   button2.position(150, canvasy+16)
   button2.class('button');
   button2.mouseClicked(addbot);
+
+  var nn1 = new network();
+  nn1.init([1,1]);
+  nn1.fp();
+  noStroke();
 }
 //////////////////////////////////////////////////////////////////////////
 function draw() {
 
   frameRate(6)
   background('#b3b3b3');
-  worldrun();
+  worldrun1();
 
   for (i in foods) {
     foods[i].show();
@@ -31,6 +39,8 @@ function draw() {
     bots[i].move();
     bots[i].show();
   }
+
+  worldrun2();
 
 }
 //////////////////////////////////////////////////////////////////////////
@@ -60,12 +70,14 @@ function bot() {
   this.move = function() {
     this.x += 1;
     this.location = [this.x*50,this.y*50];
+    this.hunger -= 5;
   }
 }
 
 function addfood() {
   foods.push(new food());
   foods[foods.length-1].init();
+
 }
 
 function addbot() {
@@ -73,7 +85,7 @@ function addbot() {
   bots[bots.length-1].init();
 }
 
-function worldrun() {
+function worldrun1() {
   for (b in bots) {
     bots[b].hunger -= 10;
     for (f in foods) {
@@ -83,4 +95,89 @@ function worldrun() {
       }}
     if (bots[b].hunger <= 0) {
       bots.splice(b,1);
-      }}}
+      }
+    }
+}
+
+function worldrun2() {
+  for (x=0;x<grid[0];x++) {
+    for (y=0;y<grid[1];y++) {
+      //if nothing
+      if (String(get(x*50,y*50)) == String([179, 179, 179, 255])) {
+        worldmap[y*grid[0]+x] = 0;
+      }
+      //if food
+      else if (String(get(x*50,y*50)) == String([255, 0, 0, 255])) {
+        worldmap[y*grid[0]+x] = 2;
+      }
+      //if bot
+      else if (String(get(x*50,y*50)) == String([0, 0, 0, 255])) {
+        worldmap[y*grid[0]+x] = 3;
+      }
+    }
+  }
+}
+
+
+//Nerual Network
+function neuron() {
+  this.init = function(input) {
+    this.w = Math.random();
+    this.b = Math.random();
+    this.activation = 'sigmoid';
+    this.input = input;
+    this.output;
+  }
+  this.activate = function(activation) {
+    if (activation == 'sigmoid') {
+      this.output = 1 / (1 + Math.E ** -this.output);
+    }
+  }
+  this.fp = function() {
+    this.output = 0
+    this.output += this.w * arrSum(this.input) + this.b;
+    this.activate(this.activation);
+  }
+}
+
+function layer() {
+  this.init = function(inputlayer,numofneuron) {
+    this.inputlayer = inputlayer;
+    this.numofneuron = numofneuron;
+    this.neuron = [];
+    this.outputlayer;
+    for (i=0;i<this.numofneuron;i++) {
+      this.neuron.push(new neuron())
+      this.neuron[this.neuron.length-1].init(inputlayer);
+    }
+  }
+  this.fp = function() {
+    this.outputlayer = [];
+    for (i=0;i<this.numofneuron;i++) {
+      this.neuron[i].fp();
+      this.outputlayer.push(this.neuron[i].output);
+    }
+  }
+}
+
+function network() {
+  this.init = function(dimension) {
+    this.dimension = dimension;
+    this.layer = [];
+    this.inputdata;
+    this.output;
+    for (i2=0;i2<(this.dimension.length);i2+=1) {
+      this.layer.push(new layer())
+      this.layer[this.layer.length-1].init([1],this.dimension[i2]);
+    }
+  }
+  this.loaddata = function(data) {
+    this.inputdata = data;
+  }
+  this.fp = function() {
+    for (i3=0;i3<this.layer.length;i3++) {
+      this.layer[i3].fp();
+    }
+    this.output = this.layer[this.layer.length-1].outputlayer;
+  }
+}
